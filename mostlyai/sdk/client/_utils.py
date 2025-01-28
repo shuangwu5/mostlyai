fd32265903de70f68144ab27d30e41c6e34aa5cc
Michael Platzer <michael.platzer@gmail.com>
2025-01-28 10:34:25 +0100

    feat: handle GeneratorListItems for probe and generate

--- mostlyai/sdk/client/_utils.py
+++ mostlyai/sdk/client/_utils.py
@@ -39,6 +39,7 @@ from mostlyai.sdk.domain import (
     SyntheticTableConfig,
     Connector,
     SyntheticDataset,
+    GeneratorListItem,
 )
 from mostlyai.sdk.client._naming_conventions import map_camel_to_snake_case
 
@@ -174,7 +175,7 @@ def harmonize_sd_config(
     size = size if size is not None else {}
     seed = seed if seed is not None else {}
 
-    if isinstance(generator, Generator):
+    if isinstance(generator, Generator | GeneratorListItem):
         generator_id = str(generator.id)
     elif generator is not None:
         generator_id = str(generator)
