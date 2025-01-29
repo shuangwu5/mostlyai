89d4f208c546291779e37f13ba81f930544bc910
Shuang Wu <shuang.wu@mostly.ai>
2025-01-29 21:08:55 +0100

    update installation script

--- install_bnb_vllm_rocm.sh
+++ install_bnb_vllm_rocm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# build bitsandbytes from source for MI200s (gfx90a) / MI300 (gfx942) GPUs
 git clone --recurse https://github.com/ROCm/bitsandbytes
 cd bitsandbytes
 git checkout rocm_enabled_multi_backend
@@ -9,6 +10,7 @@ make
 uv pip install .
 cd ..
 
+# build vllm from source for MI200s (gfx90a) / MI300 (gfx942) GPUs
 git clone https://github.com/vllm-project/vllm.git
 source .venv/bin/activate
 uv pip install /opt/rocm/share/amd_smi
@@ -17,3 +19,9 @@ cd vllm
 uv pip install -r requirements-rocm.txt
 PYTORCH_ROCM_ARCH="gfx90a;gfx942" python setup.py develop
 cd ..
+
+# set environment variables for performance optimization
+# ref: https://rocm.docs.amd.com/en/latest/how-to/rocm-for-ai/inference-optimization/workload.html#performance-environment-variables
+export HIP_FORCE_DEV_KERNARG=1
+export NCCL_MIN_NCHANNELS=112
+export TORCH_BLAS_PREFER_HIPBLASLT=1
