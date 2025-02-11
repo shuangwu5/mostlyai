6eb9450cb3bb703d7b1c9d7f98c430bf98bf458a
mostlyai-ci <37445847+mostlyai-ci@users.noreply.github.com>
2025-02-11 14:55:44 +0400

    Release 4.1.1 (#196)

--- mostlyai/sdk/__init__.py
+++ mostlyai/sdk/__init__.py
@@ -15,4 +15,4 @@
 from mostlyai.sdk.client.api import MostlyAI
 
 __all__ = ["MostlyAI"]
-__version__ = "4.1.0"  # Do not set this manually. Use poetry version [params].
+__version__ = "4.1.1"  # Do not set this manually. Use poetry version [params].
